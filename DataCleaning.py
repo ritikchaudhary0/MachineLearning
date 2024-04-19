8c89c0e27efc7b30f78e13ba49f5f3a0f7ee0c15
RITIK CHAUDHARY <ritikchaudhary408@gmail.com>
2024-04-19 18:35:09 +0530

    DataCleaning.py

--- DataCleaning.py
+++ DataCleaning.py
@@ -85,4 +85,31 @@ for row in moma:
     row[3] = clean_begindate
     row[4] = clean_enddate
 
-#
+# Parsing Numbers from Complex Strings, Part One
+# This column contains data in many different formats:
+"""Some years are in parentheses.
+Some years have c. or C. before them, indicating that the year is approximate.
+Some have year ranges, indicated with a dash.
+Some have 's to indicate a decade.
+"""
+
+test_data = ["1912", "1929", "1913-1923",
+             "(1951)", "1994", "1934",
+             "c. 1915", "1995", "c. 1912",
+             "(1988)", "2002", "1957-1959",
+             "c. 1955.", "c. 1970's", 
+             "C. 1990-1999"]
+
+bad_chars = ["(",")","c","C",".","s","'", " "]
+def strip_characters(string):
+    for bad_char in bad_chars:
+        string = string.replace(bad_char,"")
+    return string
+
+stripped_test_data = []
+for str in test_data:
+    cleaned_str = strip_characters(str)
+    stripped_test_data.append(cleaned_str)
+
+
+# 
