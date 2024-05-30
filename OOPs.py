adcc5363681ce7c6595d714aa7853bcb9a818213
RITIK CHAUDHARY <ritikchaudhary408@gmail.com>
2024-05-30 15:52:59 +0530

    OOPs.py

---
+++ OOPs.py
@@ -0,0 +1,14 @@
+# object-oriented programming (OOP). Rather than designing code around sequential steps, we will design it around objects. 
+l = [1, 2, 3]
+s = "string"
+d = {"a": 1, "b": 2}
+print(type(l))
+print(type(s))
+print(type(d))
+
+'''
+Output- <class 'list'>
+<class 'str'>
+<class 'dict'>
+'''
+
