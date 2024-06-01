ce7eb061f3d5c0227a5315a9e9fb5122e84e3d61
RITIK CHAUDHARY <ritikchaudhary408@gmail.com>
2024-06-01 16:53:14 +0530

    OOPs.py

--- OOPs.py
+++ OOPs.py
@@ -12,3 +12,19 @@ Output- <class 'list'>
 <class 'dict'>
 '''
 
+# Instantiating a Class--------------------------------------------------------------------------------------------------------------------------------------------------------
+globals().clear() # It is a Python method that clears all the global variables from the current global namespace.
+class MyClass:
+    pass
+class MyClass:
+    pass
+
+my_instance = MyClass()
+print(type(my_instance))
+
+
+# Creating Methods---------------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
+In order to make our class do something, we need to define some methods. Methods allow objects to perform actions.
+You can think of methods like special functions that belong to a particular class. This is why we call the replace method str.replace()— because the method belongs to the str class.
+
